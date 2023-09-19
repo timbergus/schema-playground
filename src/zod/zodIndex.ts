ad3d94de5562112723c7ff1f1b76ce5a50e72f93
Gustavo Muñoz <timbergus@gmail.com>
2023-09-19 19:40:05 +0200

    Remove comments that are in the README.

--- src/zod/zodIndex.ts
+++ src/zod/zodIndex.ts
@@ -2,10 +2,6 @@ import { z } from 'zod'
 import { Model } from '../types'
 import { numberValidator, stringValidator } from './rules'
 
-// After making a field optional (including null), we can use a refine method
-// to make it required. So, the moment the city is required, the validations
-// fail (we show a message in the UI).
-
 export const getSchema = (model?: Model) =>
   z
     .object({
@@ -14,24 +10,15 @@ export const getSchema = (model?: Model) =>
       age: numberValidator(model?.isAgeRequired),
       city: stringValidator(model?.isCityRequired),
       address: z.object({
-        // We need to do it optional if we have an external condition.
-        // We will validate it using the additional condition in the
-        // superRefine method.
         street: stringValidator(),
       }),
     })
     .superRefine((value, { addIssue }) => {
       if (
-        // External condition.
         value.name === 'Gustavo' &&
-        // If the value is empty.
         !value.address?.street &&
-        // Then we check if the value is required according
-        // to the model.
         model?.isStreetRequired
       ) {
-        // Finally we set the error. We use a custom error
-        // attached to the field.
         addIssue({
           code: z.ZodIssueCode.custom,
           path: ['address', 'street'],
